6b35dc460a2a04a371a40f3835477df0f14e8c5f
Estelle Choi <estele.choi@gmail.com>
2020-07-28 22:42:17 +0900

    texts in template got dynamic

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,17 +2,17 @@
   <footer class="section">
     <div class="container">
       <ul class="site-information">
-        <li>배드해빗클럽</li>
-        <li>02-960-0000</li>
-        <li>cs@badhabitclub.io</li>
-        <li>우리은행 1005-000-000000 채영빈</li>
-        <li>서울시 동대문구 휘경로2길 19 지하 1층</li>
+        <li>{{ siteInfo.title }}</li>
+        <li>{{ siteInfo.telNo }}</li>
+        <li>{{ siteInfo.email }}</li>
+        <li>{{ siteInfo.bankAcct }} {{ siteInfo.bankAcctHldrNm }}</li>
+        <li>{{ siteInfo.address }}</li>
       </ul>
 
       <ul class="site-information">
-        <li>사업자등록번호 000-00-00000</li>
-        <li>통신판매업신고번호 제2020-서울동대문-0384호 대표 채영빈</li>
-        <li>개인정보보호책임자 채영빈</li>
+        <li>사업자등록번호 {{ siteInfo.coRegNo }}</li>
+        <li>통신판매업신고번호 {{ siteInfo.mailOrderRptNo }} 대표 {{ siteInfo.rpsntNm }}</li>
+        <li>개인정보보호책임자 {{ siteInfo.rpsntNm }}</li>
         <li>
           <a>이용약관</a>
         </li>
@@ -25,7 +25,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      siteInfo: {
+        title: "배드해빗클럽",
+        telNo: "02-960-0000",
+        email: "cs@badhabitclub.io",
+        bankAcct: "우리은행 1005-000-000000",
+        bankAcctHldrNm: "채영빈",
+        address: "서울시 동대문구 휘경로2길 19 지하 1층",
+        coRegNo: "000-00-00000",
+        mailOrderRptNo: "제2020-서울동대문-0384호",
+        rpsntNm: "채영빈",
+      },
+    };
+  },
+};
 </script>
 
 <style>
