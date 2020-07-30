caff728615d9006389709d3e30b4398d163696ff
Estelle Choi <estele.choi@gmail.com>
2020-07-30 23:52:11 +0900

    class name changed

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="signin-box">
-    <header class="signin-box__header">
-      <h1 class="signin-box__title">로그인</h1>
+  <div class="sign-box sign-box--short">
+    <header class="sign-box__header">
+      <h1 class="sign-box__title">로그인</h1>
     </header>
 
-    <section class="section signin-box__body" tabindex="0">
+    <section class="section sign-box__body" tabindex="0">
       <div class="container">
         <div class="img-box" v-if="isMember">
           <div class="img-box__display">
@@ -14,14 +14,15 @@
 
         <form action="#" method="post" id="signin-form">
           <div class="container">
-            <fieldset class="signin-fieldset">
-              <!-- isMember -->
-              <label for="signin-email" class="signin-fieldset__item" v-if="!isMember">
+            <fieldset class="sign-fieldset">
+              <!-- aria-invalid attr value will be injected dynamically -->
+              <label for="signin-email" class="sign-fieldset__item" v-if="!isMember">
                 <input
                   type="email"
                   class="input--text"
                   id="signin-email"
                   aria-label="이메일"
+                  aria-required="true"
                   autocomplete="off"
                   autocorrect="off"
                   spellcheck="false"
@@ -30,12 +31,13 @@
                 />
               </label>
 
-              <label for="signin-password" class="signin-fieldset__item">
+              <label for="signin-password" class="sign-fieldset__item">
                 <input
                   type="password"
                   class="input--text"
                   id="signin-password"
                   aria-label="비밀번호"
+                  aria-required="true"
                   autocomplete="off"
                   autocorrect="off"
                   spellcheck="false"
@@ -43,7 +45,7 @@
                   placeholder="비밀번호"
                 />
               </label>
-              <button type="submit" aria-busy="false" class="btn signin-fieldset__item">로그인하기</button>
+              <button type="submit" aria-busy="false" class="btn sign-fieldset__item">로그인하기</button>
             </fieldset>
 
             <div>
@@ -54,8 +56,9 @@
       </div>
     </section>
 
-    <footer class="signin-box__footer">
-      <button typ="button" aria-busy="false" class="btn btn--primary">다른 계정 사용하기</button>
+    <footer class="sign-box__footer">
+      <button typ="button" aria-busy="false" class="btn btn--primary" v-if="isMember">다른 계정 사용하기</button>
+      <button typ="button" aria-busy="false" class="btn btn--primary">회원가입</button>
     </footer>
   </div>
 </template>
