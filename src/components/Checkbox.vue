e8e18231e4fb284b55f0c3365e8d4cd1d22985c7
Estelle Choi <estele.choi@gmail.com>
2020-08-01 19:04:16 +0900

    checkbox input and label got componentized

---
+++ src/components/Checkbox.vue
@@ -0,0 +1,60 @@
+<template>
+  <label :for="id" class="text-box">
+    <span class="checkbox" :class="{ 'checkbox--checked' : model }">
+      <input
+        type="checkbox"
+        aria-invalid="false"
+        class="input--checkbox"
+        :id="id"
+        :value="value"
+        v-model="model"
+        @change="change"
+      />
+    </span>
+    <span class="checkbox-label">{{ label ? label : '' }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "checkbox",
+  props: {
+    value: {
+      type: String,
+      default() {
+        return "";
+      },
+    },
+    checked: {
+      type: Boolean,
+      default() {
+        return false;
+      },
+    },
+    id: {
+      type: String,
+    },
+    label: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      model: false,
+    };
+  },
+  watch: {
+    checked(newVal) {
+      this.model = newVal;
+    },
+  },
+  methods: {
+    change() {
+      this.$emit("change", this.model);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
