0e9815d01c865b66c7808c0cc1ddfb6d6a26823b
Estelle Choi <estele.choi@gmail.com>
2020-08-01 19:02:54 +0900

    component mixins added

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import { router } from "./router/index";
 
+import componentMixins from "./components/componentMixins";
+Vue.mixin(componentMixins);
+
 Vue.config.productionTip = false;
 
 new Vue({
