1b8817a27bbf5e593b61730c49afb097d9261486
Estelle Choi <estele.choi@gmail.com>
2020-08-02 21:03:15 +0900

    Issue -- how to emit appropriate event and data

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,72 +1,72 @@
 <template>
-  <div class="sign-box sign-box--short">
-    <header class="sign-box__header">
-      <h1 class="sign-box__title">로그인</h1>
-    </header>
-
-    <section class="section sign-box__body" tabindex="0">
-      <div class="container">
-        <div class="img-box" v-if="isMember">
-          <div class="img-box__display">
-            <img :src="img.src" :alt="img.title" :title="img.title" />
-          </div>
-        </div>
-
-        <form action="#" method="post" id="signin-form">
-          <div class="container">
-            <fieldset class="sign-fieldset">
-              <!-- aria-invalid attr value will be injected dynamically -->
-              <label for="signin-email" class="sign-fieldset__item" v-if="!isMember">
-                <input
-                  type="email"
-                  class="input--text"
-                  id="signin-email"
-                  aria-label="이메일"
-                  aria-required="true"
-                  autocomplete="off"
-                  autocorrect="off"
-                  spellcheck="false"
-                  autocapitalize="none"
-                  placeholder="이메일"
-                />
-              </label>
-
-              <label for="signin-password" class="sign-fieldset__item">
-                <input
-                  type="password"
-                  class="input--text"
-                  id="signin-password"
-                  aria-label="비밀번호"
-                  aria-required="true"
-                  autocomplete="off"
-                  autocorrect="off"
-                  spellcheck="false"
-                  autocapitalize="none"
-                  placeholder="비밀번호"
-                />
-              </label>
-              <div class="sign-fieldset__item sign-fieldset__item--auto-height">
-                <checkbox
-                  label="로그인 상태 유지"
-                  id="signin-check-keep-online"
-                  @change="toggleKeepOnline"
-                ></checkbox>
-              </div>
-              <button type="submit" aria-busy="false" class="btn sign-fieldset__item">로그인하기</button>
-            </fieldset>
-
-            <div>
-              <button type="button" aria-busy="false" class="btn btn--primary">비밀번호를 잊으셨나요?</button>
-              <button typ="button" aria-busy="false" class="btn btn--primary" @click="goSignup">회원가입</button>
-            </div>
-          </div>
-        </form>
+  <div class="container">
+    <div class="img-box" v-if="isMember">
+      <div class="img-box__display">
+        <img :src="img.src" :alt="img.title" :title="img.title" />
       </div>
-    </section>
+    </div>
 
-    <footer class="sign-box__footer">
-      <button typ="button" aria-busy="false" class="btn btn--primary" v-if="isMember">다른 계정 사용하기</button>
-    </footer>
+    <form action="#" method="post" id="signin-form">
+      <div class="container">
+        <fieldset class="sign-fieldset">
+          <!-- aria-invalid attr value will be injected dynamically -->
+          <label for="signin-email" class="sign-fieldset__item" v-if="!isMember">
+            <input
+              type="email"
+              class="input--text"
+              id="signin-email"
+              aria-label="이메일"
+              aria-required="true"
+              autocomplete="off"
+              autocorrect="off"
+              spellcheck="false"
+              autocapitalize="none"
+              placeholder="이메일"
+            />
+          </label>
+
+          <label for="signin-password" class="sign-fieldset__item">
+            <input
+              type="password"
+              class="input--text"
+              id="signin-password"
+              aria-label="비밀번호"
+              aria-required="true"
+              autocomplete="off"
+              autocorrect="off"
+              spellcheck="false"
+              autocapitalize="none"
+              placeholder="비밀번호"
+            />
+          </label>
+
+          <div class="sign-fieldset__item sign-fieldset__item--auto-height">
+            <checkbox label="로그인 상태 유지" id="signin-check-keep-online" @change="toggleKeepOnline"></checkbox>
+          </div>
+
+          <button
+            type="submit"
+            aria-busy="false"
+            class="btn sign-fieldset__item"
+            @click="signIn"
+          >로그인하기</button>
+        </fieldset>
+
+        <div class="esc-btn-box">
+          <button
+            type="button"
+            aria-busy="false"
+            class="btn btn--primary"
+            :class="{ 'btn--disabled' : isLoading }"
+            :disabled="isLoading"
+            :aria-disabled="isLoading"
+            @click="goForgotPassword"
+          >비밀번호를 잊으셨나요?</button>
+
+          <button typ="button" aria-busy="false" class="btn btn--primary" @click="goSignUp">회원가입</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -77,22 +77,27 @@ export default {
     return {
       ariaBusy: false,
       img: {
-        src: "./../static/brand.png",
+        src: "./img/brand.png",
         title: "유진",
       },
       isMember: false,
+      isLoading: false,
     };
   },
   methods: {
     toggleKeepOnline(val) {
       console.log(val);
     },
-    goSignup() {
-      this.$emit("close", "signup");
+    goSignUp() {
+      this.$emit("close", "signUp");
+    },
+    goForgotPassword() {
+      this.$emit("close", "forgotPassword");
+    },
+    signIn(evt) {
+      evt.preventDefault();
+      this.$emit("close", "signIn");
     },
   },
 };
 </script>
-
-<style>
-</style>
