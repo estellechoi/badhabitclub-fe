f7426293a573e92093ccae7a8c87794ce298d1b8
Estelle Choi <estele.choi@gmail.com>
2020-08-02 21:00:58 +0900

    routers added

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Home from "../views/Home.vue";
+import Home from "./../views/Home.vue";
+import SetPassword from "./../views/SetPassword.vue";
+import GoodsList from "./../views/GoodsList.vue";
 
 Vue.use(VueRouter);
 
@@ -12,6 +14,16 @@ export const router = new VueRouter({
 			path: "/",
 			name: "home",
 			component: Home,
+		},
+		{
+			path: "/setPassword",
+			name: "setPassword",
+			component: SetPassword,
+		},
+		{
+			path: "/goodsList",
+			name: "goodsList",
+			component: GoodsList,
 		},
 	],
 });
