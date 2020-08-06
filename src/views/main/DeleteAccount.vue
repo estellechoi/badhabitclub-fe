ebe74df97ac4deac2db33360da15dcaa4e508061
Estelle Choi <estele.choi@gmail.com>
2020-08-07 05:12:33 +0900

    계정 비활성화 페이지

---
+++ src/views/main/DeleteAccount.vue
@@ -0,0 +1,26 @@
+<template>
+  <main role="main">
+    <section class="section section--delete">
+      <div class="container">
+        <div class="centering-box">계정을 비활성화하려는 이유는 무엇인가요?</div>
+
+        <fieldset>
+          <legend>계정 비활성화 이유 선택지 그룹</legend>
+          <div>
+            <label for>
+              <span>사생활 보호에 관한 우려가 있어요.</span>
+              <input type="radio" aria-invalid="false" />
+            </label>
+          </div>
+        </fieldset>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
