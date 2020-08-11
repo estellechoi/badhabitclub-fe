5752910feb150968f3965c123ea9b661f2569f2e
Estelle Choi <estele.choi@gmail.com>
2020-08-11 22:47:27 +0900

    alternative text was hidden using clip css

--- src/views/modal/SignUp.vue
+++ src/views/modal/SignUp.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="sign-box">
     <header class="sign-box__header">
-      <h1 class="sign-box__title">회원가입</h1>
+      <h1 class="sign-box__title">
+        <span class="blind-box">회원가입</span>
+      </h1>
     </header>
 
     <section class="section sign-box__body" tabindex="0">
