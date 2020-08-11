04e9f82a60d8bdfc0cc7f247c74bb54ed8a781d7
Estelle Choi <estele.choi@gmail.com>
2020-08-11 22:49:16 +0900

    goods related pages will be rendered under this component

---
+++ src/views/main/Goods.vue
@@ -0,0 +1,16 @@
+<template>
+  <main role="main">
+    <section class="section section--goods">
+      <div class="container">
+        <router-view></router-view>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
