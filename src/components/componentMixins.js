c396841f81b120fa8f5fed40aab894b53b57bac7
Estelle Choi <estele.choi@gmail.com>
2020-08-12 22:35:15 +0900

    new components added

--- src/components/componentMixins.js
+++ src/components/componentMixins.js
@@ -1,9 +1,13 @@
 import Checkbox from "./Checkbox.vue";
 import RadioGroup from "./RadioGroup.vue";
+import SelectBox from "./SelectBox.vue";
+import Spinner from "./Spinner.vue";
 
 export default {
 	components: {
 		Checkbox,
 		RadioGroup,
+		SelectBox,
+		Spinner,
 	},
 };
