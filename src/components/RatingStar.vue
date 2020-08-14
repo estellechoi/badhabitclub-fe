f7b2b51f49f40a0759bc06e1bb581a1209eb198e
Estelle Choi <estele.choi@gmail.com>
2020-08-14 17:32:01 +0900

    Rating star component with star icons

---
+++ src/components/RatingStar.vue
@@ -0,0 +1,42 @@
+<template>
+  <span :class="boxClass">
+    <a>
+      <span class="rating-star">
+        <i
+          class="fas fa-star"
+          v-for="(item, index) in [1,2,3,4,5]"
+          :key="index"
+          :class="{ 'rating-star-icon--gray' : item > starCnt }"
+        ></i>
+      </span>
+
+      <span class="review-cnt">{{ ratersCnt }} {{ label }}</span>
+    </a>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    boxClass: {
+      type: String,
+      default: "",
+    },
+    starCnt: {
+      type: Number,
+      default: 0,
+    },
+    ratersCnt: {
+      type: Number,
+      default: 0,
+    },
+    label: {
+      type: String,
+      default: " 개 리뷰",
+    },
+  },
+};
+</script>
+
+<style>
+</style>
