a7cfe56ceb68dafe67c0892c0e2fc8fdaea3dc06
Estelle Choi <estele.choi@gmail.com>
2020-08-24 23:24:18 +0900

    쇼핑백/주문정보 및 결제/완료 페이지를 하위에 라우팅할 뷰 컴포넌트

---
+++ src/views/main/Order.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <progress-header :list="progressList"></progress-header>
+
+    <main role="main">
+      <section class="section section--order">
+        <div class="container">
+          <router-view @select-list="selectList" :bag-info="bagInfo"></router-view>
+        </div>
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "order",
+  data() {
+    return {
+      progressList: [
+        { label: "쇼핑백", path: "bag" },
+        { label: "주문정보 및 결제", path: "checkout" },
+        { label: "완료", path: "complete" },
+      ],
+      bagInfo: null,
+    };
+  },
+  methods: {
+    selectList(val) {
+      this.bagInfo = val;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
