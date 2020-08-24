6a335a65006a95d96a6a825ba25411fa93d366eb
Estelle Choi <estele.choi@gmail.com>
2020-08-24 23:21:34 +0900

    상품상세/쇼핑백 페이지 등에서 사용할 Price tag 컴포넌트화

---
+++ src/components/PriceTag.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="containerClass">
+    <div class="prdt-origin-price">
+      <span class="discount-rate">{{ dscntRate * 100 }}%</span>
+      <span class="origin-price">
+        <del>{{ originPrice | addCommas }}</del>
+      </span>
+    </div>
+
+    <div class="prdt-sale-price">
+      <span class="total-price">
+        ₩
+        <strong>{{ salePrice | addCommas }}</strong>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "price-tag",
+  props: {
+    dscntRate: {
+      type: Number,
+      default: 0,
+    },
+    originPrice: {
+      type: Number,
+      default: 0,
+    },
+    salePrice: {
+      type: Number,
+      default: 0,
+    },
+    fontSize: {
+      type: Number,
+      default: 12,
+    },
+    containerClass: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
