ea0faf8809b98c7f175fd2a6637c3f1e032c86e8
Estelle Choi <estele.choi@gmail.com>
2020-08-24 23:17:35 +0900

    text cell용 테이블로 변경

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,65 +1,76 @@
 <template>
-  <table class="table">
-    <caption class="table-caption" :class="captionClass">{{ caption }}</caption>
+	<table class="table">
+		<caption class="table-caption" :class="captionClass">
+			{{
+				caption
+			}}
+		</caption>
 
-    <colgroup>
-      <col
-        class="table-col"
-        :style="`width: ${100 / 6}%`"
-        v-for="(col, colIndex) in columns"
-        :key="colIndex"
-      />
-    </colgroup>
+		<colgroup>
+			<col
+				class="table-col"
+				:style="`width: ${100 / columns.length}%`"
+				v-for="(col, colIndex) in columns"
+				:key="colIndex"
+			/>
+		</colgroup>
 
-    <thead>
-      <tr class="table-head-row">
-        <th v-for="(col, colIndex) in columns" :key="colIndex">{{ col.title }}</th>
-      </tr>
-    </thead>
+		<thead>
+			<tr class="table-head-row">
+				<th v-for="(col, colIndex) in columns" :key="colIndex">
+					{{ col.title }}
+				</th>
+			</tr>
+		</thead>
 
-    <tbody>
-      <tr class="table-body-row" v-for="(item, index) in rowData" :key="index">
-        <td v-for="(col, colIndex) in columns" :key="colIndex">
-          <span :title="cellTitle(item, col)">{{ cellText(item, col) }}</span>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+		<tbody>
+			<tr class="table-body-row" v-for="(item, index) in rowData" :key="index">
+				<td v-for="(col, colIndex) in columns" :key="colIndex">
+					<span
+						:title="cellTitle(item, col)"
+						v-html="cellContent(item, col)"
+					></span>
+				</td>
+			</tr>
+		</tbody>
+	</table>
 </template>
 
 <script>
 export default {
-  name: "custom-table",
-  props: {
-    caption: {
-      type: String,
-      default: "",
-    },
-    captionClass: {
-      type: String,
-      default: "",
-    },
-    columns: {
-      type: Array,
-      default() {
-        return [];
-      },
-    },
-    rowData: {
-      type: Array,
-      default() {
-        return [];
-      },
-    },
-  },
-  methods: {
-    cellTitle(item, col) {
-      return `${col.title} ${this.cellText(item, col)}`;
-    },
-    cellText(item, col) {
-      const targetKey = Object.keys(item).filter((key) => col.data === key);
-      return targetKey.length ? item[targetKey[0]] : "";
-    },
-  },
+	name: "custom-table",
+	props: {
+		caption: {
+			type: String,
+			default: "",
+		},
+		captionClass: {
+			type: String,
+			default: "",
+		},
+		columns: {
+			type: Array,
+			default() {
+				return [];
+			},
+		},
+		rowData: {
+			type: Array,
+			default() {
+				return [];
+			},
+		},
+	},
+	methods: {
+		cellTitle(item, col) {
+			return `${col.title} ${this.cellContent(item, col)}`;
+		},
+		cellContent(item, col) {
+			const targetKey = Object.keys(item).filter((key) => col.data === key);
+			if (!targetKey.length) return "";
+			if (col.formatter) return col.formatter(item[targetKey[0]], item);
+			return item[targetKey[0]];
+		},
+	},
 };
 </script>
