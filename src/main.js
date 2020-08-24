7219f6d65562f89a9f2041599f036d8a31252dc5
Estelle Choi <estele.choi@gmail.com>
2020-08-24 23:15:43 +0900

    filter 추가

--- src/main.js
+++ src/main.js
@@ -1,14 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router/index";
+import router from "./router/index.js";
+import store from "./store/index.js";
+
+import filter from "./filter.js";
+filter.init(Vue);
 
 import componentMixins from "./components/componentMixins";
 Vue.mixin(componentMixins);
 
 Vue.config.productionTip = false;
-// Vue.prototype.$bus = new Vue();
 
 new Vue({
 	render: (h) => h(App),
 	router,
+	store,
 }).$mount("#app");
