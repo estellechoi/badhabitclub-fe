f98e98a5764b5237a2f24f0356ffe7dcc685d43e
Estelle Choi <estele.choi@gmail.com>
2020-08-24 23:28:02 +0900

    계정 삭제 이유 선택/확인/완료 하위 뷰가 라우팅될 뷰 컴포넌트

--- src/views/main/DeleteAccount.vue
+++ src/views/main/DeleteAccount.vue
@@ -1,36 +1,38 @@
 <template>
-  <div>
-    <progress-header :list="progressList"></progress-header>
+	<div>
+		<progress-header :list="progressList"></progress-header>
 
-    <main role="main">
-      <section class="section section--delete">
-        <div class="container">
-          <router-view @select-reason="storeReason" :storedReason="storedReason"></router-view>
-        </div>
-      </section>
-    </main>
-  </div>
+		<main role="main">
+			<section class="section section--delete">
+				<div class="container">
+					<router-view
+						@select-reason="storeReason"
+						:storedReason="storedReason"
+					></router-view>
+				</div>
+			</section>
+		</main>
+	</div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      progressList: [
-        { label: "이유 선택", path: "reasons" },
-        { label: "확인", path: "confirm" },
-        { label: "완료", path: "complete" },
-      ],
-      storedReason: null,
-    };
-  },
-  methods: {
-    storeReason(val) {
-      this.storedReason = val;
-    },
-  },
+	data() {
+		return {
+			progressList: [
+				{ label: "이유 선택", path: "reasons" },
+				{ label: "확인", path: "confirm" },
+				{ label: "완료", path: "complete" },
+			],
+			storedReason: null,
+		};
+	},
+	methods: {
+		storeReason(val) {
+			this.storedReason = val;
+		},
+	},
 };
 </script>
 
-<style>
-</style>
+<style></style>
