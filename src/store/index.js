66cef8fa353397cb16149bd01bc358c571349d65
Estelle Choi <estele.choi@gmail.com>
2020-08-27 22:34:17 +0900

    백엔드 개발전 일부 기능 테스트를 위한 가짜 코드

--- src/store/index.js
+++ src/store/index.js
@@ -5,21 +5,50 @@ Vue.use(Vuex);
 export default new Vuex.Store({
 	state: {
 		bagList: [],
+		orderList: [],
+		userInfo: {
+			userName: "최유진",
+			email: "estele.choi@gmail.com",
+			level: "LV001",
+		},
 	},
 	getters: {
-		bagList(state) {
-			return state.bagList;
-		},
+		bagList: (state) => state.bagList,
+		orderList: (state) => state.orderList,
+		userInfo: (state) => state.userInfo,
 	},
 	mutations: {
 		ADD_BAG_LIST(state, payload) {
 			state.bagList = state.bagList.concat(payload);
+		},
+		REMOVE_BAG_ITEM(state, payload) {
+			let cnt = 0;
+			state.bagList.some((item, index) => {
+				if (payload.indexOf(item.bagId) > -1) {
+					state.bagList.splice(index, 1);
+					cnt += 1;
+					if (cnt >= payload.length) return true;
+				}
+			});
+		},
+		ADD_ORDER_LIST(state, payload) {
+			state.orderList.push(payload);
+		},
+		SET_USER_INFO(state, payload) {
+			state.userInfo = payload;
 		},
 	},
 	actions: {
 		FETCH_BAG_LIST(context, param) {
 			// api
 			console.log(context, param);
+		},
+		FETCH_ORDER_INFO(context, param) {
+			console.log(context, param);
+		},
+		FETCH_USER_INFO(context, param) {
+			console.log(context, param);
+			context.commit("SET_USER_INFO", param);
 		},
 	},
 });
