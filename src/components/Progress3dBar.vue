5a3c37e4275d40dd1d6135380aed9fe21926c3e9
Estelle Choi <estele.choi@gmail.com>
2020-09-07 23:58:40 +0900

    styles

--- src/components/Progress3dBar.vue
+++ src/components/Progress3dBar.vue
@@ -1,32 +1,30 @@
 <template>
-  <div :class="outerClass">
-    <div class="progress-3d-container">
-      <div class="progress-3d-bar">
-        <div class="barface barface--top">
-          <div
-            class="growing-bar"
-            :style="`width:${valuenow}%`"
-            role="progressbar"
-            :aria-valuemin="valuemin"
-            :aria-valuemax="valuemax"
-            :aria-valuenow="valuenow"
-          ></div>
-        </div>
+  <div class="progress-3d-container">
+    <div class="progress-3d-bar">
+      <div class="barface barface--top">
+        <div
+          class="growing-bar"
+          :style="`width:${valuenow}%`"
+          role="progressbar"
+          :aria-valuemin="valuemin"
+          :aria-valuemax="valuemax"
+          :aria-valuenow="valuenow"
+        ></div>
+      </div>
 
-        <div class="barface barface--side-0">
-          <div class="growing-bar" :style="`width:${valuenow}%`"></div>
-        </div>
+      <div class="barface barface--side-0">
+        <div class="growing-bar" :style="`width:${valuenow}%`"></div>
+      </div>
 
-        <div class="barface barface--floor">
-          <div class="growing-bar" :style="`width:${valuenow}%`"></div>
-        </div>
+      <div class="barface barface--floor">
+        <div class="growing-bar" :style="`width:${valuenow}%`"></div>
+      </div>
 
-        <div class="barface barface--side-a"></div>
-        <div class="barface barface--side-b"></div>
+      <div class="barface barface--side-a"></div>
+      <div class="barface barface--side-b"></div>
 
-        <div class="barface barface--side-1">
-          <div class="growing-bar" :style="`width:${valuenow}%`"></div>
-        </div>
+      <div class="barface barface--side-1">
+        <div class="growing-bar" :style="`width:${valuenow}%`"></div>
       </div>
     </div>
   </div>
@@ -36,10 +34,6 @@
 export default {
   name: "progress-3d-bar",
   props: {
-    outerClass: {
-      type: String,
-      default: "",
-    },
     valuenow: {
       type: Number,
       default: 0,
