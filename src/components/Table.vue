4e01c167f9c262543ddcbfc3cb52018309724468
Estelle Choi <estele.choi@gmail.com>
2020-09-07 23:23:56 +0900

    테이블 열별 오름/내림차순 버튼 추가

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,5 @@
 <template>
-  <table class="table">
+  <table class="table" :class="tableClass">
     <caption class="table-caption" :class="captionClass">
       {{
       caption
@@ -9,21 +9,51 @@
     <colgroup>
       <col
         class="table-col"
-        :style="`width: ${100 / columns.length}%`"
-        v-for="(col, colIndex) in columns"
+        :style="`width: ${100 / cols.length}%`"
+        v-for="(col, colIndex) in cols"
         :key="colIndex"
       />
     </colgroup>
 
     <thead>
       <tr class="table-head-row" :class="headClass">
-        <th v-for="(col, colIndex) in columns" :key="colIndex">{{ col.title }}</th>
+        <th v-for="(col, colIndex) in cols" :key="colIndex">
+          {{ col.title }}
+          <div
+            class="table-icon-box"
+            v-if="hasFilters"
+            @mouseover="col.slideIcon = true"
+            @mouseout="col.slideIcon = false"
+          >
+            <span
+              class="table-icon table-icon--asc"
+              :class="{ 'table-icon--active' : col.orderBy === 'asc'}"
+              role="button"
+              :aria-label="`${col.title} 열을 기준으로 오름차순 정렬하기`"
+              tabindex="0"
+              @click="reorderData('asc', col)"
+            >
+              <span class="table-icon__content"></span>
+            </span>
+
+            <span
+              class="table-icon"
+              :class="{ 'table-icon--desc' : col.slideIcon || col.orderBy === 'desc', 'table-icon--active' : col.orderBy === 'desc' }"
+              role="button"
+              :aria-label="`${col.title} 열을 기준으로 내림차순 정렬하기`"
+              tabindex="0"
+              @click="reorderData('desc', col)"
+            >
+              <span class="table-icon__content table-icon__content--desc"></span>
+            </span>
+          </div>
+        </th>
       </tr>
     </thead>
 
     <tbody>
       <tr class="table-body-row" :class="bodyClass" v-for="(item, index) in rowData" :key="index">
-        <td v-for="(col, colIndex) in columns" :key="colIndex">
+        <td v-for="(col, colIndex) in cols" :key="colIndex">
           <span :title="cellTitle(item, col)" v-html="cellContent(item, col)"></span>
         </td>
       </tr>
@@ -35,6 +65,14 @@
 export default {
   name: "custom-table",
   props: {
+    hasFilters: {
+      type: Boolean,
+      default: false,
+    },
+    tableClass: {
+      type: String,
+      default: "",
+    },
     caption: {
       type: String,
       default: "",
@@ -64,7 +102,24 @@ export default {
       },
     },
   },
+  data() {
+    return {
+      cols: [],
+    };
+  },
   methods: {
+    reorderData(orderBy, col) {
+      this.cols = this.cols.map((item) => {
+        if (item.data === col.data) item.orderBy = orderBy;
+        else item.orderBy = null;
+        return item;
+      });
+
+      this.$emit("reorder", {
+        orderBy,
+        col: col.data,
+      });
+    },
     cellTitle(item, col) {
       return `${col.title} ${this.cellContent(item, col)}`;
     },
@@ -77,6 +132,14 @@ export default {
 
       return col.formatter ? col.formatter(val, item) : val;
     },
+  },
+  mounted() {
+    if (this.hasFilters) {
+      this.cols = this.columns.map((item) => {
+        item.slideIcon = false;
+        return item;
+      });
+    } else this.cols = this.columns;
   },
 };
 </script>
