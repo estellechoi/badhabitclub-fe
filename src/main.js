329ca77397c5c3536f5d988e464df9c343632ba4
Estelle Choi <estele.choi@gmail.com>
2020-09-10 20:48:35 +0900

    mixins - filter 순서 변경

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from "./App.vue";
 import router from "./router/index.js";
 import store from "./store/index.js";
 
-import filter from "./filter.js";
-filter.init(Vue);
-
 import componentMixins from "./components/componentMixins";
 import CONST from "./const.js";
 
 Vue.mixin({ ...componentMixins, ...CONST });
+
+import filter from "./filter.js";
+filter.init(Vue);
 
 Vue.config.productionTip = false;
 
