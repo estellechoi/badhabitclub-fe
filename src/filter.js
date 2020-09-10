bcf2b9e1826030cc5a8784df8e6f33a9b1ec16ab
Estelle Choi <estele.choi@gmail.com>
2020-09-10 20:48:12 +0900

    공통코드 필터 추가

--- src/filter.js
+++ src/filter.js
@@ -1,8 +1,34 @@
+const commonCode = [
+	{
+		cd: "OV001",
+		label: "매출",
+	},
+	{
+		cd: "OV002",
+		label: "주문건수",
+	},
+	{
+		cd: "OV003",
+		label: "주문당 결제금액",
+	},
+	{
+		cd: "OV004",
+		label: "방문자수",
+	},
+];
+
 export default {
 	init(Vue) {
 		Vue.filter("addCommas", (val) => {
 			if (!val && val !== 0) return "";
 			return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		});
+
+		Vue.filter("codeLabel", (val) => {
+			if (!val && val !== 0) return "";
+			const codeObj = commonCode.filter((item) => item.cd === val)[0];
+			if (!codeObj) return "";
+			return codeObj.label;
 		});
 	},
 };
