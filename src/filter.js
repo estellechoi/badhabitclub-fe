cad1e94b6f31ac939c1cf9ce78f5e1304a5f1296
Estelle Choi <estele.choi@gmail.com>
2020-09-17 17:05:08 +0900

    공통코드 추가

--- src/filter.js
+++ src/filter.js
@@ -15,6 +15,58 @@ const commonCode = [
 		cd: "OV004",
 		label: "방문자수",
 	},
+	{
+		cd: "OV005",
+		label: "신규회원",
+	},
+	{
+		cd: "OV006",
+		label: "비활성 회원",
+	},
+	{
+		cd: "OV007",
+		label: "누적 회원수",
+	},
+	{
+		cd: "OV008",
+		label: "누적 주문금액",
+	},
+	{
+		cd: "OV009",
+		label: "평균 주문금액",
+	},
+	{
+		cd: "OV010",
+		label: "클럽머니",
+	},
+	{
+		cd: "OV011",
+		label: "좋아요",
+	},
+	{
+		cd: "LV001",
+		label: "Silver",
+	},
+	{
+		cd: "LV002",
+		label: "Gold",
+	},
+	{
+		cd: "LV003",
+		label: "VIP",
+	},
+	{
+		cd: "UT001",
+		label: "활성회원",
+	},
+	{
+		cd: "UT002",
+		label: "정지회원",
+	},
+	{
+		cd: "UT003",
+		label: "비활성회원",
+	},
 ];
 
 export default {
