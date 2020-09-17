d850f9bcb0d5f687a6984adf1503b80a6bfad8bc
Estelle Choi <estele.choi@gmail.com>
2020-09-17 16:59:58 +0900

    Table cell 컨텐츠 함수화

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -53,12 +53,13 @@
       </tr>
     </thead>
 
-    <tbody>
+    <tbody @click="(evt) => clickRow(evt)">
       <tr
         class="table-body-row"
         v-for="(item, index) in rowData"
         :key="index"
         :class="{ bodyClass, 'table-body-row--hover-st' : hasHoverStyle }"
+        :data-row-data="JSON.stringify(item)"
       >
         <td v-for="(col, colIndex) in cols" :key="colIndex">
           <span :title="cellTitle(item, col)" v-html="cellContent(item, col)"></span>
@@ -131,15 +132,34 @@ export default {
         col: col.data,
       });
     },
-    cellTitle(item, col) {
-      return `${col.title} ${this.cellContent(item, col)}`;
+    clickRow(evt) {
+      const target = evt.path.filter((item) => item.tagName === "TR")[0];
+      if (!target) return;
+      this.$emit("click-row", JSON.parse(target.dataset.rowData));
     },
-    cellContent(item, col) {
+    cellTitle(item, col) {
+      return `${col.title} ${this.cellText(item, col)}`;
+    },
+    cellOriginValue(item, col) {
       let val = item[Object.keys(item).filter((key) => col.data === key)[0]];
-      if (!val) return "";
+      if (!val && val !== 0) return "";
+      return val;
+    },
+    cellText(item, col) {
+      let val = this.cellOriginValue(item, col);
+
+      if (col.type && col.type === "code")
+        val = this.$options.filters.codeLabel(val);
 
       if (col.type && col.type === "number")
         val = this.$options.filters.addCommas(val);
+
+      return val;
+    },
+    cellContent(item, col) {
+      const originVal = this.cellOriginValue(item, col);
+      const text = this.cellText(item, col);
+      let val = text;
 
       if (col.type && col.type === "image")
         val = `<div class='img-box'><div class='img-box__display' style='width: ${
@@ -148,7 +168,7 @@ export default {
           item[col.alt]
         }'/></div></div>`;
 
-      return col.formatter ? col.formatter(val, item) : val;
+      return col.formatter ? col.formatter(val, originVal) : val;
     },
   },
   mounted() {
