508eb65a2dce6dd4375a3cceb3a367bd04760d41
Estelle Choi <estele.choi@gmail.com>
2020-09-27 23:44:40 +0900

    뷰 믹신즈

--- src/mixins.js
+++ src/mixins.js
@@ -1,5 +1,34 @@
 export default {
 	methods: {
+		pastDays(val) {
+			const today = new Date();
+			const year = today.getFullYear();
+			let month = today.getMonth() + 1;
+			month = month >= 10 ? month : `0${month}`;
+			let day = today.getDate();
+			day = day >= 10 ? day : `0${day}`;
+
+			val = val.split("-").join("");
+
+			return parseInt(`${year}${month}${day}`) - parseInt(val);
+		},
+		selectImgFile(evt, img) {
+			const files = Array.from(evt.target.files);
+			const accept = evt.target.accept;
+
+			if (!this.validateFileType(files, accept))
+				return window.alert(`${accept.toString()} 이미지만 업로드 가능합니다.`);
+
+			// 썸네일
+			const objUrl = URL.createObjectURL(files[0]);
+			img.src = objUrl;
+			img.onload = () => URL.revokeObjectURL(objUrl);
+
+			return files;
+		},
+		validateFileType(files, accept) {
+			return files.every((file) => accept.includes(file.type));
+		},
 		animateNumbers(els) {
 			els.forEach((el) => {
 				const updateCount = () => {
