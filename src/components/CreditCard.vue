793ca640c9144a13f2eb5aa086c0b8f12326ec7b
Estelle Choi <estele.choi@gmail.com>
2020-09-27 23:46:45 +0900

    신용카드 애니메이션 컴포넌트

---
+++ src/components/CreditCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="credit-card-box" aria-hidden="true">
+    <div class="flip">
+      <div class="front">
+        <div class="chip"></div>
+      </div>
+
+      <div class="back">
+        <div class="mag-strip"></div>
+        <div class="cvc"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
